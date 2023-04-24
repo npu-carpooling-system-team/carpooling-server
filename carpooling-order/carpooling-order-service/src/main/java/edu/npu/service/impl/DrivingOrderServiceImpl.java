8eb6f95b7734cad2a74afbec475834cbf41ceb9f
tanknok <1633434682@qq.com>
2023-04-24 20:38:43 +0800

    更新log输出方式

--- carpooling-order/carpooling-order-service/src/main/java/edu/npu/service/impl/DrivingOrderServiceImpl.java
+++ carpooling-order/carpooling-order-service/src/main/java/edu/npu/service/impl/DrivingOrderServiceImpl.java
@@ -8,9 +8,10 @@ import edu.npu.feignClient.UserServiceClient;
 import edu.npu.mapper.OrderMapper;
 import edu.npu.service.DrivingOrderService;
 import edu.npu.vo.R;
-import groovy.util.logging.Slf4j;
+
 import jakarta.annotation.Resource;
 import edu.npu.common.OrderStatusEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
@@ -31,13 +32,13 @@ public class DrivingOrderServiceImpl extends ServiceImpl<OrderMapper, Order>
         Order order = getById(orderId);
 
         if (order == null) {
-            log.error("按orderID查询订单失败，订单不存在，订单id：{" + orderId + "}");
+            log.error("按orderID查询订单失败，订单不存在，订单id：{}", orderId);
             return R.error("订单不存在");
         }
         User user = userServiceClient.getUserByAccountUsername(loginAccount.getUsername());
 
         if (user == null) {
-            log.error("按username查询用户失败，用户不存在，username：{" + loginAccount.getUsername() + "}");
+            log.error("按username查询用户失败，用户不存在，username：{}",loginAccount.getUsername() );
             return R.error("用户不存在");
         }
         order.setStatus(OrderStatusEnum.DRIVING_USER_CONFIRM_DEPARTURE.getValue());
@@ -45,7 +46,7 @@ public class DrivingOrderServiceImpl extends ServiceImpl<OrderMapper, Order>
         if(updateSuccess){
             return R.ok("乘客确认发车成功");
         }else{
-            log.error("乘客确认发车失败，订单id：{" + orderId + "}");
+            log.error("乘客确认发车失败，订单id：{}",orderId);
             return R.error("乘客确认发车失败，MySQL数据库异常");
         }
     }
@@ -55,13 +56,13 @@ public class DrivingOrderServiceImpl extends ServiceImpl<OrderMapper, Order>
         Order order = getById(orderId);
 
         if (order == null) {
-            log.error("按orderID查询订单失败，订单不存在，订单id：{" + orderId + "}");
+            log.error("按orderID查询订单失败，订单不存在，订单id：{}",orderId);
             return R.error("订单不存在");
         }
         User user = userServiceClient.getUserByAccountUsername(loginAccount.getUsername());
 
         if (user == null) {
-            log.error("按username查询用户失败，用户不存在，username：{" + loginAccount.getUsername() + "}");
+            log.error("按username查询用户失败，用户不存在，username：{}",loginAccount.getUsername());
             return R.error("用户不存在");
         }
         order.setStatus(OrderStatusEnum.DRIVING_USER_CONFIRM_ARRIVED.getValue());
@@ -69,7 +70,7 @@ public class DrivingOrderServiceImpl extends ServiceImpl<OrderMapper, Order>
         if(updateSuccess){
             return R.ok("乘客确认到达成功");
         }else{
-            log.error("乘客确认到达失败，订单id：{" + orderId + "}");
+            log.error("乘客确认到达失败，订单id：{}",orderId);
             return R.error("乘客确认到达失败，MySQL数据库异常");
         }
     }
