f90e577c796087a98ac8931580e4abc27b0a1ba3
WangMinan <wangminan0811@hotmail.com>
2023-06-30 17:36:08 +0800

    紧急修正 移动nacos注册点到nacos集群

--- carpooling-security-sdk/src/main/java/edu/npu/filter/JwtAuthenticationFilter.java
+++ carpooling-security-sdk/src/main/java/edu/npu/filter/JwtAuthenticationFilter.java
@@ -160,7 +160,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     // Bearer <token>
     private String getJwtFromRequest(HttpServletRequest request) {
-
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
